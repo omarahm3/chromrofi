58085f9353c7a242182b3f3aec3fe6aabee9b42a
Omar Ahmed <me@mrg.sh>
2024-07-20 18:16:29 +0200

    feat(command): use browser api

--- cmd/root.go
+++ cmd/root.go
@@ -3,26 +3,23 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"slices"
-	"strings"
 
-	"github.com/omarahm3/chromrofi/platform/chrome"
+	"github.com/omarahm3/chromrofi/platform/browser"
 	"github.com/omarahm3/chromrofi/platform/database"
 	"github.com/spf13/cobra"
 )
 
 var (
-	orderBy   string
-	browser   string
-	limit     int
-	profile   string
-	useSearch bool
-	rootCmd   = &cobra.Command{
+	orderBy    string
+	browserArg string
+	limit      int
+	profile    string
+	useSearch  bool
+	rootCmd    = &cobra.Command{
 		Use:   "chromrofi",
 		Short: "chromrofi is a CLI for rofi to browse chrome history",
 		Run:   runCommand,
 	}
-	supportedBrowsers = []string{"chrome", "brave", "firefox"}
 )
 
 func Init() {
@@ -30,19 +27,19 @@ func Init() {
 	rootCmd.PersistentFlags().StringVarP(&profile, "profile", "p", "Default", "Chrome profile to use")
 	rootCmd.PersistentFlags().IntVarP(&limit, "limit", "l", 10, "Number of results to return")
 	rootCmd.PersistentFlags().BoolVarP(&useSearch, "use-search", "s", false, "Google if no results found")
-	rootCmd.PersistentFlags().StringVarP(&browser, "browser", "b", "chrome", "Browser to use")
+	rootCmd.PersistentFlags().StringVarP(&browserArg, "browser", "b", "chrome", "Browser to use")
 	if err := rootCmd.Execute(); err != nil {
 		message("failed to run chromrofi", &err)
 	}
 }
 
 func runCommand(cmd *cobra.Command, args []string) {
-	if slices.Contains(supportedBrowsers, browser) == false {
-		err := fmt.Errorf("unsupported browser: %s", browser)
-		message(fmt.Sprintf("supported browsers: %s", strings.Join(supportedBrowsers, ", ")), &err)
+	if !browser.HasBrowser(browserArg) {
+		err := fmt.Errorf("unsupported browser: %s", browserArg)
+		message(fmt.Sprintf("supported browsers: %s", browser.SupportedBrowsers), &err)
 	}
 
-	c, err := chrome.GetChrome(profile)
+	c, err := browser.GetBrowser(browser.GetBrowserType(browserArg), profile)
 	if err != nil {
 		message("failed to initialize chrome instance", &err)
 	}
